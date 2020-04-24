659371a7285100fb03f97eabfd78da2e0259fc69
Alex <alexmg86@mail.ru>
2020-04-24 17:20:46 +0300

    add test for withSum method

---
+++ tests/DatabaseTestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Alexmg86\LaravelSubQuery\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class DatabaseTestCase extends TestCase
+{
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('app.debug', 'true');
+
+        $app['config']->set('database.default', 'testbench');
+
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+}
