c1a99c0c00f81e068b4e1a52a30596a333f0b6fc
Turkin Maksim <neo@lna.org.ru>
2020-05-03 02:25:43 +0300

    Tests codestyle enhancements
    Models moved to separate files.
    setUp method moved to base class

---
+++ tests/Models/Invoice.php
@@ -0,0 +1,29 @@
+<?php
+namespace Alexmg86\LaravelSubQuery\Tests\Models;
+
+use Alexmg86\LaravelSubQuery\Traits\LaravelSubQueryTrait;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use LaravelSubQueryTrait;
+
+    public $table = 'invoices';
+    public $timestamps = false;
+    protected $guarded = ['id'];
+
+    public function items()
+    {
+        return $this->hasMany(Item::class, 'invoice_id');
+    }
+
+    public function goods()
+    {
+        return $this->hasMany(Good::class, 'invoice_id');
+    }
+
+    public function allGoods()
+    {
+        return $this->goods()->withoutGlobalScopes();
+    }
+}
